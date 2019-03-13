20ae609ae47185cc5f6bfc6f88b1aa469b452511
Peter Onneby <ponneby@xebia.fr>
2019-03-13 21:52:15 +0100

    Passing first characterization test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,11 +1,26 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TripServiceTest {
 
+    private static final User NO_LOGGED_USER = null;
+
     @Test
-    void should_() {
-        new TripService().getTripsByUser(null);
+    public void should_throw_exception_when_user_not_logged_in() {
+        assertThrows(UserNotLoggedInException.class, () -> getTestableTripService().getTripsByUser(null));
+    }
+
+    private TripService getTestableTripService() {
+        return new TripService() {
+            @Override
+            protected User getLoggedUser() {
+                return NO_LOGGED_USER;
+            }
+        };
     }
 }
