ecf652df761ad1a02055ffedfbd60f4b65e13382
Peter Onneby <ponneby@xebia.fr>
2019-03-13 21:34:44 +0100

    First failing characterization test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,11 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.jupiter.api.Test;
+
 public class TripServiceTest {
-	
+
+    @Test
+    void should_() {
+        new TripService().getTripsByUser(null);
+    }
 }
