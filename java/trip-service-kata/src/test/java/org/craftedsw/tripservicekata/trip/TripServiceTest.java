7fd9362ba1c2c3cb936b4a4df2ed8670b857b302
ONNEBY Peter <ONNEBY@ad.agf.fr>
2020-09-17 17:07:43 +0200

    [WIP] Test exception

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,25 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TripServiceTest {
-	
+    @Test
+    void should_throw_exception_when_no_logged_user() {
+        assertThrows(UserNotLoggedInException.class, () ->
+                new TestableTripService().getTripsByUser(null));
+    }
+
+    
+
+    private class TestableTripService extends TripService {
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
+    }
 }
