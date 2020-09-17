2b4edb843abeeac29d2e856b0273a43d32eff9ba
CHAABANI Anis <CHAABAA@ad.agf.fr>
2020-09-17 17:38:31 +0200

    Test no logged user and user no friends

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -5,21 +5,34 @@ import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TripServiceTest {
+    public static User loggedUser;
+
     @Test
     void should_throw_exception_when_no_logged_user() {
+        loggedUser = null;
         assertThrows(UserNotLoggedInException.class, () ->
                 new TestableTripService().getTripsByUser(null));
     }
 
-    
+    @Test
+    void should_return_no_trips_when_user_has_friends() {
+        loggedUser = new User();
+        TestableTripService testableTripService = new TestableTripService();
+        List<Trip> tripsByUser = testableTripService.getTripsByUser(new User());
+        assertEquals(Collections.EMPTY_LIST, tripsByUser);
+    }
 
     private class TestableTripService extends TripService {
         @Override
         protected User getLoggedUser() {
-            return null;
+            return loggedUser;
         }
     }
 }
